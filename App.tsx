25decf3771a000f9dee6fcac6027cbdec643814f
Joshua Gornall <josh@Joshuas-MacBook-Pro.local>
2023-11-01 13:04:01 +0800

    app set up

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from './src/navigation/AppContainer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <AppContainer/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/navigation/AppContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AppContainer = () => {
+  return (
+      <div>
+          <text>AppContainer hello world</text>
+      </div>
+  );
+}
+
+AppContainer.propTypes = {}
+
+export default AppContainer
